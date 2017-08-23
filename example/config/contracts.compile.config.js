52cfc5199dcf2d80ff6b02230f12961b9031b712
lukehedger <luke.hedger@gmail.com>
2017-08-23 10:54:09 +0100

    Update config options: - Add comments to config options - Split outputPath into path and filename - Use filename + .json when writing file to disk - Use inputPath if outputPath not specified - Add contractFileExtension to config - Use contractFileExtension when reading file from disk

--- example/config/contracts.compile.config.js
+++ example/config/contracts.compile.config.js
@@ -1,9 +1,54 @@
 const path = require('path')
 
 module.exports = {
+  /**
+   * contractFileExtension
+   *
+   * File extension of the target contracts
+   *
+   * @default sol
+   * @type {String}
+   */
+  contractFileExtension: 'sol',
+
+  /**
+   * contracts
+   *
+   * The contract files to be compiled
+   *
+   * @type {Array}
+   */
   contracts: [
-    'Echo.sol',
+    'Echo',
   ],
+
+  /**
+   * inputPath
+   *
+   * Directory to read target contracts from
+   *
+   * @type {String}
+   */
   inputPath: path.join(__dirname, '../contracts'),
-  outputPath: path.join(__dirname, '../contracts/contracts.json'),
+
+  /**
+   * outputFilename
+   *
+   * Filename for the compiler output
+   *
+   * @default contracts
+   * @type {String}
+   */
+  outputFilename: 'contracts',
+
+  /**
+   * outputPath
+   *
+   * Directory to write compiled contracts to
+   * Will inherit `inputPath` if undefined
+   *
+   * @default `inputPath`
+   * @type {String}
+   */
+  outputPath: path.join(__dirname, '../contracts'),
 }
