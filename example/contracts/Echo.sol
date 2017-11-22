91da8a5d49b491bb146369c55e840d81baef7b5e
lukehedger <luke.hedger@gmail.com>
2017-11-22 17:09:21 +0000

    Update example with 0.4.18 pragma

--- example/contracts/Echo.sol
+++ example/contracts/Echo.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.18;
 
 contract Echo {
   event Echoed(uint echoValue);
 
-  function echo(uint _value) constant returns (uint) {
+  function echo(uint _value) public returns (uint) {
     Echoed(_value);
 
     return _value;
